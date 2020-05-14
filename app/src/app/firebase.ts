d5c08ee7f8d201cbd1789a0c1c7e0f62878c4947
Rohit Mitra <45285730+Mitter-Fox@users.noreply.github.com>
2020-05-14 15:57:40 +0530

    Add files via upload

---
+++ app/src/app/firebase.ts
@@ -0,0 +1,13 @@
+
+const config = {
+    apiKey: "AIzaSyDbSueLuSKiHdabRffopQ2geOfqDkTsgmE",
+    authDomain: "social-app1234.firebaseapp.com",
+    databaseURL: "https://social-app1234.firebaseio.com",
+    projectId: "social-app1234",
+    storageBucket: "social-app1234.appspot.com",
+    messagingSenderId: "874924804463",
+    appId: "1:874924804463:web:6cf994c56e16b967e6c09a",
+    measurementId: "G-VCF8R0R09M"
+};
+
+export default config
